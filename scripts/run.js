eefd43d0a38db976e7a7a39a4e887500d9ddec30
Kristaps Grinbergs <fassko@gmail.com>
2022-04-05 17:05:00 +0300

    Comments in run script

--- scripts/run.js
+++ scripts/run.js
@@ -9,6 +9,7 @@ const main = async () => {
   console.log("addr1", addr1.address);
   console.log("addr2", addr2.address);
 
+  // deploy the contract
   const NFTTallinn = await hre.ethers.getContractFactory("NFTTallinn");
   const nftTallinn = await NFTTallinn.deploy("NFTTallinn", "NFTTLN");
 
@@ -16,21 +17,31 @@ const main = async () => {
 
   console.log("NFT Tallinn ticket deployed:", nftTallinn.address);
 
-  await nftTallinn.createTicket(addr1.address, 1, "https://bafybeigsc6eqcgxkevnvxcvbdpdmecxsn4vteio4kd5tsdiubjj2ohmysu.ipfs.dweb.link/");
+  // create the ticket
+  // upload metadata to NFTStorage or Pinata
+  await nftTallinn.createTicket(
+    addr1.address,
+    1, // Ticket type - VIP
+    "https://bafybeigsc6eqcgxkevnvxcvbdpdmecxsn4vteio4kd5tsdiubjj2ohmysu.ipfs.dweb.link/"
+  );
 
+  // .connect(addr1) - connects with the concrete address
   const myTicket = await nftTallinn.connect(addr1).getMyTicket();
   console.log(myTicket);
 
+  // check entract - owner cheks it for the addr1
   let checkEntrance = await nftTallinn.checkEntrance(addr1.address);
   console.log("Check entrance", checkEntrance);
 
+  // sell to addr2
   const sellTicket = await nftTallinn.connect(addr1).sellTicket(addr2.address);
-
   console.log(sellTicket);
 
+  // check entrance for addr1 - not allowed anymore
   checkEntrance = await nftTallinn.checkEntrance(addr1.address);
   console.log("Check entrance", checkEntrance);
 
+  // addr2 now allowed to enter
   checkEntrance = await nftTallinn.checkEntrance(addr2.address);
   console.log("Check entrance", checkEntrance);
 };
